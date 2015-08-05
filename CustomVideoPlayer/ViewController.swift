28d51d489eea78ed4e8b645f565758f0b725caab
Gavin Conway <gavin@binarymosaic.com>
2015-08-05 12:07:56 +1000

    Implemented invisible stop/start button

--- CustomVideoPlayer/ViewController.swift
+++ CustomVideoPlayer/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     var avPlayerView: UIView = UIView();
     var avPlayer: AVPlayer = AVPlayer();
     var avPlayerLayer: AVPlayerLayer!
+    var invisibleButton: UIButton = UIButton();
 
     override func viewDidLoad()
     {
@@ -22,6 +23,9 @@ class ViewController: UIViewController {
 
         self.avPlayerLayer = AVPlayerLayer(player: avPlayer)
         self.view.layer.insertSublayer(avPlayerLayer, atIndex: 0)
+
+        self.invisibleButton.addTarget(self, action: "invisibleButtonTapped:", forControlEvents: UIControlEvents.TouchUpInside)
+        self.view.addSubview(invisibleButton)
 
         let url = NSURL(string: "http://content.jwplatform.com/manifests/vM7nH0Kl.m3u8");
         let playerItem = AVPlayerItem(URL: url)
@@ -37,11 +41,22 @@ class ViewController: UIViewController {
     {
         super.viewWillLayoutSubviews()
         self.avPlayerLayer.frame = self.view.bounds;
+        self.invisibleButton.frame = self.view.bounds;
     }
 
     override func supportedInterfaceOrientations() -> Int
     {
         return Int(UIInterfaceOrientationMask.Landscape.rawValue)
+    }
+
+    func invisibleButtonTapped(sender: UIButton!)
+    {
+        var playerIsPlaying:Bool = self.avPlayer.rate > 0
+        if (playerIsPlaying) {
+            self.avPlayer.pause();
+        } else {
+            self.avPlayer.play();
+        }
     }
 
 }
