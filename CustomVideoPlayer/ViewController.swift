7ce9ee9539a60b09e1a190401c95cefd2ee78cb6
Gavin Conway <gavin@binarymosaic.com>
2015-08-05 13:46:02 +1000

    Added time remaining counter

--- CustomVideoPlayer/ViewController.swift
+++ CustomVideoPlayer/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
     var avPlayer: AVPlayer = AVPlayer();
     var avPlayerLayer: AVPlayerLayer!
     var invisibleButton: UIButton = UIButton();
+    var timeRemainingLabel: UILabel = UILabel();
+    var timeObserver: AnyObject!;
 
     override func viewDidLoad()
     {
@@ -30,6 +32,23 @@ class ViewController: UIViewController {
         let url = NSURL(string: "http://content.jwplatform.com/manifests/vM7nH0Kl.m3u8");
         let playerItem = AVPlayerItem(URL: url)
         self.avPlayer.replaceCurrentItemWithPlayerItem(playerItem)
+
+        self.timeRemainingLabel.textColor = UIColor.whiteColor()
+        self.view.addSubview(self.timeRemainingLabel);
+
+        let timeInterval: CMTime = CMTimeMakeWithSeconds(1.0, 10)
+        self.timeObserver = self.avPlayer.addPeriodicTimeObserverForInterval(timeInterval, queue: dispatch_get_main_queue()) { (elapsedTime: CMTime) -> Void in
+            let duration = CMTimeGetSeconds(self.avPlayer.currentItem.duration);
+            if (isfinite(duration)) {
+                let timeRemaining = CMTimeGetSeconds(self.avPlayer.currentItem.duration) - CMTimeGetSeconds(elapsedTime)
+                self.timeRemainingLabel.text = String(format: "%02.f:%02.f", (floor(timeRemaining / 60)) % 60, timeRemaining % 60)
+            }
+        }
+    }
+
+    deinit
+    {
+        self.avPlayer.removeTimeObserver(self.timeObserver)
     }
 
     override func viewWillAppear(animated: Bool)
@@ -42,6 +61,8 @@ class ViewController: UIViewController {
         super.viewWillLayoutSubviews()
         self.avPlayerLayer.frame = self.view.bounds;
         self.invisibleButton.frame = self.view.bounds;
+        let timeRemainingLabelY = self.view.bounds.size.height - 25;
+        self.timeRemainingLabel.frame = CGRectMake(0, timeRemainingLabelY, 60, 25)
     }
 
     override func supportedInterfaceOrientations() -> Int
